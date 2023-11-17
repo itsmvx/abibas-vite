ac0855f4cd7c72ad00a15cb2591217b47da6868e
xenovzx <xenovzx@gmail.com>
2023-11-17 21:35:33 +0700

    Lib folder added

---
+++ src/lib/ImageLib.jsx
@@ -0,0 +1,75 @@
+import Haruna1L from "/assets/7a27660bedbdbbc4b8a9125ed546f7b8.webp"
+import Haruna2L from "/assets/8244049dd7746ef76bf566cc1ca50a02.webp"
+import Haruna3L from "/assets/92443010ed7746ef76bf566cc1ca50a02.webp"
+import Haruna4L from "/assets/dec12f052b66040e64714a18354ea03d.webp"
+import Noa1L from "/assets/91e0bdf8e4a8af77250a9f6dbedc0bf2.webp"
+import Noa2L from "/assets/20230702_185119.webp"
+import Noa3L from "/assets/20230710_090318.webp"
+import Noa1P from "/assets/20230422_005858.webp"
+import Mari1L from "/assets/20230715_1315444.webp"
+import Mari2L from "/assets/20230815_130224.webp"
+import Rio1L from "/assets/20230830_205049.webp"
+import Rio2L from "/assets/20231002_105450.webp"
+import Shiroko1L from "/assets/20230905_150623.webp"
+import AronaPlana1P from "/assets/20230909_213827.webp"
+import HaloFest1L from "/assets/36e1064e4ded68c9017a024cfe408dad.webp"
+import HaloFest2L from "/assets/6df6a8e49e15bd7dc8a343a567ca59f3.webp"
+import AbibasLogo from "/assets/abibas-logo.webp"
+export const ImagesAll = {
+    Haruna1L,
+    Haruna2L,
+    Haruna3L,
+    Haruna4L,
+    Noa1L,
+    Noa2L,
+    Noa3L,
+    Noa1P,
+    Mari1L,
+    Mari2L,
+    Rio1L,
+    Rio2L,
+    Shiroko1L,
+    AronaPlana1P,
+    HaloFest1L,
+    HaloFest2L,
+}
+
+export {
+    AbibasLogo,
+    Haruna1L,
+    Haruna2L,
+    Haruna3L,
+    Haruna4L,
+    Noa1L,
+    Noa2L,
+    Noa3L,
+    Noa1P,
+    Mari1L,
+    Mari2L,
+    Rio1L,
+    Rio2L,
+    Shiroko1L,
+    AronaPlana1P,
+    HaloFest1L,
+    HaloFest2L,
+}
+export const ImagesHaruna = [
+    Haruna1L,
+    Haruna2L,
+    Haruna3L,
+    Haruna4L,
+]
+export const ImagesMari = [
+    Mari1L,
+    Mari2L,
+]
+export const ImagesNoa = [
+    Noa1L,
+    Noa2L,
+    Noa3L,
+    Noa1P,
+]
+export const ImagesRio = [
+    Rio1L,
+    Rio2L,
+]
---
+++ src/lib/StaticDataLib.jsx
@@ -0,0 +1,24 @@
+const DefaultStoreFeeds = [
+    { name: 'Ahmad Roblok'},
+    { name: 'Java Spring Boot'},
+    { name: 'Sunda Autumn Sneakers'},
+    { name: 'Sepatu Rahmat'}
+]
+const DefaultHomeSeries = {
+    haruna: {
+        tittle: "Kurodate Haruna Series",
+        description: "She buntut looks at the atmosphere of her meals, how they are prepared, what ingredients and tools are used, where she eats. The idea is that through all of this, her food will taste even better."
+    },
+    noa: {
+        tittle: "Ushio Noa Series",
+        description: "Noa is the secretary of the student council \"Seminar\" at Millennium Science School. She has a very good memory and can memorize almost everything she sees or hears once, including decisions made at the \"Seminar\" and records of the contents of meetings."
+    }
+}
+
+const DefaultHomeEvent = {
+
+}
+
+export {
+    DefaultStoreFeeds, DefaultHomeSeries, DefaultHomeEvent
+}
