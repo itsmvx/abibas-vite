54c4cd07cd364972c1f45e05d113c4589df94b37
xenovzx <xenovzx@gmail.com>
2023-11-19 04:10:24 +0700

    utils/AxiosFetch added

---
+++ src/utils/AxiosFetch.jsx
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+const fetchFeedsData = async () => {
+    console.log(import.meta.env.VITE_API_URL)
+    try {
+        return await axios.get(import.meta.env.VITE_API_URL + '/api/latest-series')
+    } catch (error){
+        throw new Error('Error Fetching Feeds Data');
+    }
+}
+const fetchFeedsUpdate = async () => {
+    try {
+        return await axios.get(import.meta.env.VITE_API_URL + '/api/store-update')
+
+    } catch (error){
+        throw new Error('Error Fetching Feeds Update');
+    }
+}
+const fetchProductsData = async (searchParams = {}) => {
+    try {
+        return await axios.get(import.meta.env.VITE_API_URL + '/products', {
+            params: searchParams
+        })
+    } catch (error) {
+        throw new Error('Error Fetching Products Data');
+    }
+}
+export {
+    fetchFeedsData,
+    fetchFeedsUpdate,
+    fetchProductsData
+}
